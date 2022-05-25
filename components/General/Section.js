8450aab03d25d01c42a89fc9fc63fddd56e88d4d
Anders Schultz <mail@anders-schultz.dk>
2022-05-25 22:12:10 +0100

    Added buyhdao page

--- components/General/Section.js
+++ components/General/Section.js
@@ -2,11 +2,12 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const Section = ({ blok }) => (
   <div>
-    <section className="Section bg-purple-200 rounded md:mx-24 lg:mx-32 m-8 mt-0 p-4" {...storyblokEditable(blok)}>
-      <h1 className="font-bungee font-bold text-3xl text-center md:text-left">
+    <section className="Section rounded md:mx-24 lg:mx-32 m-8 mt-0 p-4" style={Object.assign({}, blok.bg_color && { backgroundColor: blok.bg_color })} {...storyblokEditable(blok)}>
+      { blok.header && (<h1 className="font-bungee font-bold text-3xl text-center md:text-left">
         {blok.header}
-      </h1>
-      <div className={`grid grid-cols-${blok.cols ? blok.cols : '1'} md:grid-cols-${blok.cols_md ? blok.cols_md : '1'} lg:grid-cols-${blok.cols_lg ? blok.cols_lg : '1'} gap-2 md:gap-5 lg:gap-2 py-5`}>
+      </h1>)
+      }
+      <div className={`grid gap-${blok.gap ? blok.gap : '2' } md:gap-${blok.gap_md ? blok.gap_md : '4' } lg:gap-${blok.gap_lg ? toString(blok.gap_lg) : '6' } grid-cols-${blok.cols ? blok.cols : '1'} md:grid-cols-${blok.cols_md ? blok.cols_md : '1'} lg:grid-cols-${blok.cols_lg ? blok.cols_lg : '1'} py-5`}>
         { blok.blocks
         ? blok.blocks.map((blok) => (
             <StoryblokComponent blok={blok} key={blok._uid} />
