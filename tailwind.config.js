69e677a653ac34e82a8a960126a7a969eca7236d
Anders Schultz <mail@anders-schultz.dk>
2022-05-26 17:48:35 +0100

    added safelist to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    'md:gap-8',
+    {
+      pattern: /bg-(red|green|blue)-(100|200|300)/,
+    },
+    { 
+      pattern: /grid-cols-.+/,
+      variants: ['lg', 'md'],
+    },
+    { 
+      pattern: /gap-.+/,
+      variants: ['lg', 'md'],
+    }
+  ],
   theme: {
     extend: {
       fontFamily: {
