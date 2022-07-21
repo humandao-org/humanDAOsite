3756926626918a98ec7703047078fbf0454aee49
Kyla Gail <kylagail.azucena.iics@ust.edu.ph>
2022-07-21 23:09:31 +0800

    added contact section

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,20 @@ export default function Ambassador({ story }) {
 
   return (
     <div className="bg-[#F8F3F3]">
+      <Head>
+        <meta property="og:title" content="Ambassador Program" />
+        <meta property="og:site_name" content="humanDAO" />
+        <meta property="og:image" content="https://res.cloudinary.com/daljbo1q0/image/upload/v1653621760/hdao-nft/hdao_nft_essxni.png" />
+        <meta name="description" content="Support the underserved and get paid to help spread the word about this novel NFT project." />
+        <meta name="twitter:image" content="https://res.cloudinary.com/daljbo1q0/image/upload/v1653621760/hdao-nft/hdao_nft_essxni.png" />
+        <meta name="twitter:description" content="Support the underserved and get paid to help spread the word about this novel NFT project." />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#c5bcff" />
+        <link rel="icon" href="https://secureservercdn.net/198.71.233.86/cb6.757.myftpupload.com/wp-content/uploads/2021/12/HDAO-logo-transp-60x60-1.png?time=1651092601" sizes="32x32" />
+        <link rel="icon" href="https://secureservercdn.net/198.71.233.86/cb6.757.myftpupload.com/wp-content/uploads/2021/12/HDAO-logo-transp-60x60-1.png?time=1651092601" sizes="192x192" />
+        <link rel="apple-touch-icon" href="https://secureservercdn.net/198.71.233.86/cb6.757.myftpupload.com/wp-content/uploads/2021/12/HDAO-logo-transp-60x60-1.png?time=1651092601" />
+        <title>Ambassador Program</title>
+      </Head>
       <div className="relative z-10 mx-auto w-full">
         <nav className="sticky z-20 top-0 px-6 py-6 bg-[#F8F3F3]">
           <div className="mx-auto flex w-full max-w-[1100px] items-center justify-between gap-5">
@@ -71,12 +85,8 @@ export default function Ambassador({ story }) {
         <header className="py-32 px-5 lg:pt-52 lg:pb-80">
           <div className="mx-auto flex w-full max-w-[1100px] flex-col items-center justify-between gap-16 lg:flex-row lg:gap-5">
             <div className="w-full max-w-[548px] text-center lg:text-left">
-              <h1 className="mb-7 font-primary text-4xl font-extrabold tracking-[0.46px] text-primary sm:text-5xl md:text-6xl lg:mb-3">
-                Join the PANFT
-                <br className="hidden sm:inline-block" />
-                Ambassadors
-              </h1>
-              <p className="mb-7 text-xl font-semibold leading-[30px] -tracking-default text-dark md:text-2xl lg:mb-3">Support the underserved and get paid to help spread the word about this novel NFT project.</p>
+              <h1 className="mb-7 font-primary text-4xl font-extrabold tracking-[0.46px] text-primary sm:text-5xl md:text-6xl lg:mb-3">Join the PANFT Ambassadors</h1>
+              <p className="mb-7 text-xl font-semibold leading-[30px] tracking-normal text-dark md:text-2xl lg:mb-3">Support the underserved and get paid to help spread the word about this novel NFT project.</p>
               <Link href="/ambassador-registration">
                 <a className="mx-auto block w-fit rounded-full bg-accent-purple px-8 py-4 text-base font-bold text-white shadow-sm transition-all duration-200 hover:shadow-none md:px-10 md:text-xl lg:mx-0">Become an Ambassador</a>
               </Link>
@@ -92,40 +102,37 @@ export default function Ambassador({ story }) {
 
           <div className="mx-auto flex w-full max-w-[1100px] flex-wrap gap-8">
             <div className="min-w-[250px] flex-1 overflow-hidden rounded-lg bg-card bg-cover px-5 pt-8 pb-14 text-center shadow-circle">
-              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#F8F8F8] text-2xl font-extrabold leading-none shadow-circle">✏️</div>
+              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#ebebeb] text-3xl font-extrabold leading-none shadow-circle">✏️</div>
               <h3 className="mb-6 text-3.5xl font-bold leading-none text-black">1. Sign up️</h3>
               <p className="mx-auto w-full max-w-[230px] text-xl font-medium text-black">Start by filling out the form to get your Ambassador link</p>
             </div>
 
             <div className="min-w-[250px] flex-1 overflow-hidden rounded-lg bg-card bg-cover px-5 pt-8 pb-14 text-center shadow-circle">
-              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#F8F8F8] text-2xl font-extrabold leading-none shadow-circle">✨</div>
+              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#ebebeb] text-3xl font-extrabold leading-none shadow-circle">✨</div>
               <h3 className="mb-6 text-3.5xl font-bold leading-none text-black">2. Work your magic</h3>
               <p className="w-full text-xl font-medium text-black">Utilize PANFT as something new to share with your community</p>
             </div>
 
             <div className="min-w-[250px] flex-1 overflow-hidden rounded-lg bg-card bg-cover px-5 pt-8 pb-14 text-center shadow-circle">
-              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#F8F8F8] text-2xl font-extrabold leading-none shadow-circle">💸</div>
+              <div className="mx-auto mb-6 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#ebebeb] text-3xl font-extrabold leading-none shadow-circle">💸</div>
               <h3 className="mb-6 text-3.5xl font-bold leading-none text-black">3. Earn and grow</h3>
               <p className="mx-auto w-full max-w-[267px] text-xl font-medium text-black">Earn fees for every person using your link to mint a PANFT</p>
             </div>
           </div>
           <Link href="/ambassador-registration">
-            <a href="#" className="mx-auto mt-[72px] block w-fit rounded-full bg-accent-purple px-8 py-4 text-base font-bold text-white shadow-sm transition-all duration-200 hover:shadow-none sm:px-10 md:text-xl">
+            <a href="/ambassador-registration" className="mx-auto mt-[72px] block w-fit rounded-full bg-accent-purple px-8 py-4 text-base font-bold text-white shadow-sm transition-all duration-200 hover:shadow-none sm:px-10 md:text-xl">
               Become an Ambassador
             </a>
           </Link>
         </section>
 
-        <section id="faqs" className="faqs px-5 pb-40 md:pb-96">
+        <section id="faqs" className="faqs px-5 pb-40 md:pb-72">
           <h2 className="mb-14 text-center text-3.5xl font-extrabold leading-none text-primary md:mb-28 md:text-5xl">FAQs</h2>
 
           <div className="mx-auto w-full max-w-[870px] space-y-12 md:space-y-20">
             <div className="faq-box group" open>
               <button className="faq-btn grid w-full grid-cols-[1fr_max-content] gap-2 text-left text-base font-bold leading-none text-black md:text-3.5xl">
-                <span>
-                  What is an Ambassador?
-                  <img src="assets/images/saluting-face.png" className="inline-block h-7 w-7 object-contain align-middle" alt="" />
-                </span>
+                <span>What is an Ambassador?</span>
                 <svg className="block h-6 w-6 transform transition-all duration-200 group-open:rotate-180 md:h-8 md:w-8" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M23.75 9.677L15 17.2036L6.25 9.677" stroke="#111827" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
@@ -140,7 +147,7 @@ export default function Ambassador({ story }) {
 
             <div className="faq-box group" open>
               <button className="faq-btn grid w-full grid-cols-[1fr_max-content] gap-2 text-left text-base font-bold leading-none text-black md:text-3.5xl">
-                <span>How does an Ambassador earn? 📣</span>
+                <span>How does an Ambassador earn?</span>
                 <svg className="block h-6 w-6 transform transition-all duration-200 group-open:rotate-180 md:h-8 md:w-8" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M23.75 9.677L15 17.2036L6.25 9.677" stroke="#111827" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
@@ -148,33 +155,30 @@ export default function Ambassador({ story }) {
               <div className="pointer-events-none invisible max-h-0 text-sm font-medium leading-[30px] text-black opacity-0 transition-all duration-300 group-only:opacity-100 group-open:pointer-events-auto group-open:visible group-open:mt-4 group-open:max-h-[1080px] group-open:opacity-100 md:text-xl md:group-open:mt-9">
                 <p>
                   The Ambassador shares their unique PANFT link with their community. Everyone that reserves AND ends up minting a PANFT will earn the Ambassador a % of the mint fee. REMEMBER, just because someone reserves a spot does NOT mean they
-                  ended up minting an NFT. People who place reservations can withdraw their funds and give up their spot in line any time before the 400th reservation and the subsequent mint.
-                  <strong>PANFT will only launch and be minted after receiving 400 reservations.</strong>
+                  ended up minting an NFT. People who place reservations can withdraw their funds and give up their spot in line any time before the 400th reservation and the subsequent mint.{" "}
+                  <b>PANFT will only launch and be minted after receiving 400 reservations.</b>
                 </p>
               </div>
             </div>
 
             <div className="faq-box group" open>
               <button className="faq-btn grid w-full grid-cols-[1fr_max-content] gap-2 text-left text-base font-bold leading-none text-black md:text-3.5xl">
-                <span>
-                  What does an Ambassador earn?
-                  <img src="assets/images/coin.png" className="inline-block h-6 w-6 object-contain align-middle" alt="" />
-                </span>
+                <span>What does an Ambassador earn?</span>
                 <svg className="block h-6 w-6 transform transition-all duration-200 group-open:rotate-180 md:h-8 md:w-8" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M23.75 9.677L15 17.2036L6.25 9.677" stroke="#111827" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </button>
               <div className="pointer-events-none invisible max-h-0 text-sm font-medium leading-[30px] text-black opacity-0 transition-all duration-300 group-only:opacity-100 group-open:pointer-events-auto group-open:visible group-open:mt-4 group-open:max-h-[1080px] group-open:opacity-100 md:text-xl md:group-open:mt-9">
                 <p>
-                  For every reservation that ends up minting a PANFT through an Ambassador link,
-                  <strong>the Ambassador will receive 10% of the mint fee in the underlying token.</strong> For example, if the reservation/mint cost is 3,333 USDC, the referring Ambassadors will receive 333 USDC.
+                  For every reservation that ends up minting a PANFT through an Ambassador link, <b>the Ambassador will receive 10% of the mint fee in the underlying token.</b> For example, if the reservation/mint cost is 3,333 USDC, the referring
+                  Ambassadors will receive 333 USDC.
                 </p>
               </div>
             </div>
 
             <div className="faq-box group" open>
               <button className="faq-btn grid w-full grid-cols-[1fr_max-content] gap-2 text-left text-base font-bold leading-none text-black md:text-3.5xl">
-                <span>When does an Ambassador get paid? 🤑 </span>
+                <span>When does an Ambassador get paid?</span>
                 <svg className="block h-6 w-6 transform transition-all duration-200 group-open:rotate-180 md:h-8 md:w-8" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M23.75 9.677L15 17.2036L6.25 9.677" stroke="#111827" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
@@ -182,6 +186,22 @@ export default function Ambassador({ story }) {
               <div className="pointer-events-none invisible max-h-0 text-sm font-medium leading-[30px] text-black opacity-0 transition-all duration-300 group-only:opacity-100 group-open:pointer-events-auto group-open:visible group-open:mt-4 group-open:max-h-[1080px] group-open:opacity-100 md:text-xl md:group-open:mt-9">
                 <p>Keep in mind that we don&#39;t know when the mint and launch of PANFT will happen. It could be in 3 days, 3 weeks, or 3 months. It depends on reaching 400 reservations.</p>
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="contact pb-32">
+          <div className="mx-auto flex w-full max-w-[900px] flex-wrap px-5">
+            <div className="min-w-[250px] flex-1 overflow-hidden rounded-lg px-5 pt-8 pb-8 text-center shadow-circle bg-gradient-to-br from-zinc-200 via-rose-100 to-purple-200">
+              <h3 className="mb-2 text-3.5xl font-bold leading-none text-black">Got questions about PA NFTs?</h3>
+              <a
+                href="mailto:hdao.helpdesk@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="mx-auto mt-[72px] block w-fit rounded-full bg-accent-purple px-8 py-4 text-base font-bold text-white shadow-sm transition-all duration-200 hover:shadow-none sm:px-10 md:text-xl"
+              >
+                Contact us
+              </a>
             </div>
           </div>
         </section>
