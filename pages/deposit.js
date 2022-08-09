36ce42ab050affcf72122d44262af4cdbc9efc1f
Anders Schultz <mail@anders-schultz.dk>
2022-08-09 21:37:27 +0100

    change of wording

--- pages/deposit.js
+++ pages/deposit.js
@@ -628,7 +628,7 @@ export default function Reservation({ story }) {
               className="cursor-pointer text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 m-1 inline-block" 
               onClick={() => setShowModalWallet(true) }
             >
-              Connect
+              Connect wallet
             </div>
           </li>
         )}
